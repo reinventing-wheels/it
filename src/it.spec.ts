600ab45e56d661dc89e9724948b5499cb49dbd6b
foo <5144598+bar@users.noreply.github.com>
2018-10-26 00:02:37 +0300

    tests: improvements

--- src/it.spec.ts
+++ src/it.spec.ts
@@ -1,11 +1,48 @@
-import { forEach, reduce, filter, map, concat, repeat, generate, range, take, drop } from './functions/curried'
-import { it as wrap } from './it'
+import { countTo, leet, add, double, isOdd } from './functions/curried.spec'
+import { forEach, reduce, filter, map, concat, repeat, take, drop } from './functions/curried'
+import { always, loop, generate, sequence, range, match } from './functions/curried'
+import { It, it as wrap } from './it'
 
 describe('it', () => {
-  const countToInf = range(1)()
-  const countTo5 = range(1)(6)
+  describe('static methods', () => {
+    it('should have #always', () => {
+      const expected = [...take(10)(always(42))]
+      const received = [...It.always(42).take(10)]
+      expect(received).toEqual(expected)
+    })
 
-  describe('interface', () => {
+    it('should have #loop', () => {
+      const expected = [...take(10)(loop(leet))]
+      const received = [...It.loop(leet).take(10)]
+      expect(received).toEqual(expected)
+    })
+
+    it('should have #generate', () => {
+      const expected = [...take(10)(generate(double))]
+      const received = [...It.generate(double).take(10)]
+      expect(received).toEqual(expected)
+    })
+
+    it('should have #sequence', () => {
+      const expected = [...take(10)(sequence(double)(1))]
+      const received = [...It.sequence(double, 1).take(10)]
+      expect(received).toEqual(expected)
+    })
+
+    it('should have #range', () => {
+      const expected = [...take(10)(range()()())]
+      const received = [...It.range().take(10)]
+      expect(received).toEqual(expected)
+    })
+
+    it('should have #match', () => {
+      const expected = [...match(/./g)('foo')]
+      const received = [...It.match('foo', /./g)]
+      expect(received).toEqual(expected)
+    })
+  })
+
+  describe('methods', () => {
     it('should have #cast', () => {
       const expected = [...'foobar']
       const received = wrap('foobar').cast(it => [...it])
@@ -20,23 +57,20 @@ describe('it', () => {
     })
 
     it('should have #reduce', () => {
-      const add = (n: number, m: number) => n + m
-      const expected = reduce(add)(0)(countTo5())
-      const received = wrap(countTo5()).reduce(add, 0)
+      const expected = reduce(add)(0)(countTo(10))
+      const received = wrap(countTo(10)).reduce(add, 0)
       expect(received).toEqual(expected)
     })
 
     it('should have #filter', () => {
-      const isOdd = (n: number) => n%2 !== 0
-      const expected = [...filter(isOdd)(countTo5())]
-      const received = [...wrap(countTo5()).filter(isOdd)]
+      const expected = [...filter(isOdd)(countTo(10))]
+      const received = [...wrap(countTo(10)).filter(isOdd)]
       expect(received).toEqual(expected)
     })
 
     it('should have #map', () => {
-      const addOne = (n: number) => n + 1
-      const expected = [...map(addOne)(countTo5())]
-      const received = [...wrap(countTo5()).map(addOne)]
+      const expected = [...map(double)(countTo(10))]
+      const received = [...wrap(countTo(10)).map(double)]
       expect(received).toEqual(expected)
     })
 
@@ -73,20 +107,20 @@ describe('it', () => {
     })
 
     it('should handle infinite drop/take', () => {
-      const inf = wrap(countToInf())
+      const inf = wrap(countTo(Infinity))
       expect([...inf.take(Infinity).take(5)]).toEqual([1, 2, 3, 4, 5])
       expect([...inf.take(5).drop(Infinity)]).toEqual([])
     })
 
     it('should not close underlying iterator on drop/take', () => {
-      const inf = wrap(countToInf())
+      const inf = wrap(countTo(Infinity))
       expect([...inf.drop(2).take(2)]).toEqual([3, 4])
       expect([...inf.drop(2).take(2)]).toEqual([7, 8])
     })
 
     it('should be lazy', () => {
       const fn = jest.fn(n => n)
-      const it = wrap(generate(fn)).drop(5).take(5).filter(fn).map(fn)
+      const it = It.generate(fn).drop(5).take(5).filter(fn).map(fn)
       expect(fn).not.toHaveBeenCalled()
       expect([...it]).toEqual([5, 6, 7, 8, 9])
       expect(fn).toHaveBeenCalledTimes(20)
